30b03bd0d3257a426fe03a0ef5e8171955077a02
ak4ra <aryjna1@gmail.com>
2021-07-19 09:40:23 +0300

    Implement tests generator WIP.

--- generate_tests.go
+++ generate_tests.go
@@ -13,17 +13,19 @@ const OUTPUT_FILE = "./tests.rs"
 
 const INPUT_DIRECTORY = "./test/"
 
-// const INPUT_DIRECTORY = "./test/assignment/"
-
-func writeLine(f *os.File, text string, indentationLevel int) {
-	f.WriteString(fmt.Sprintf("%s%s\n", strings.Repeat("    ", indentationLevel), text))
+func writeLine(outputFile *os.File, text string, indentationLevel int) {
+	outputFile.WriteString(fmt.Sprintf("%s%s\n", strings.Repeat("    ", indentationLevel), text))
 }
 
-func writeTest(f *os.File, testName string, indentationLevel int) {
-	f.WriteString("\n")
-	writeLine(f, "#[test]", indentationLevel)
-	writeLine(f, fmt.Sprintf("fn %s() -> VMResult {", testName), indentationLevel)
-	writeLine(f, "}", indentationLevel)
+func writeTest(outputFile *os.File, fileInfo *fs.FileInfo, indentationLevel int) {
+	name := (*fileInfo).Name()
+	fmt.Println(name)
+
+	outputFile.WriteString("\n")
+	writeLine(outputFile, "#[test]", indentationLevel)
+	writeLine(outputFile, fmt.Sprintf("fn %s() -> VMResult {", name), indentationLevel)
+
+	writeLine(outputFile, "}", indentationLevel)
 }
 
 func writeModule(outputFile *os.File, moduleName string, moduleTestFiles []fs.FileInfo, indentationLevel int) {
@@ -39,12 +41,7 @@ func writeModule(outputFile *os.File, moduleName string, moduleTestFiles []fs.Fi
 	writeLine(outputFile, "}", indentationLevel)
 }
 
-func writeToFile() {
-	files, err := ioutil.ReadDir(INPUT_DIRECTORY)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func writeToFile(files []fs.FileInfo) {
 	f, err := os.Create(OUTPUT_FILE)
 	if err != nil {
 		log.Fatal(err)
@@ -57,20 +54,26 @@ func writeToFile() {
 	writeLine(f, "use super::*;", 1)
 	writeLine(f, "use crate::value::Value;", 1)
 
-	for _, fileOrDir := range files {
-		if fileOrDir.IsDir() {
-			// If it is a directory, create a new test module for its tests.
-			moduleName := fileOrDir.Name()
-			// dir, err := os.Open(fileOrDir.Name())
-			// if err != nil {
-			// 	log.Fatal(err)
-			// }
-			modTestFiles, err := ioutil.ReadDir(INPUT_DIRECTORY + fileOrDir.Name())
-			if err != nil {
-				log.Fatal(err)
-			}
-			writeModule(f, moduleName, modTestFiles, 1)
+	for _, fileInfo := range files {
+		// fmt.Printf("%s, isDir: %v\n", fileInfo.Name(), fileInfo.IsDir())
+		name := fileInfo.Name()
+
+		if !fileInfo.IsDir() {
+			// If it is a file, write the test in the top level module.
+			writeTest(f, &fileInfo, 1)
+			continue
 		}
+
+		// If it is a directory, create a new test module for its tests.
+		if name == "benchmark" {
+			// Exclude "benchmark" directory.
+			continue
+		}
+		modTestFilesInfo, err := ioutil.ReadDir(INPUT_DIRECTORY + name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		writeModule(f, name, modTestFilesInfo, 1)
 	}
 
 	// Closing bracket for the top level tests module.
@@ -78,7 +81,11 @@ func writeToFile() {
 }
 
 func main() {
+	files, err := ioutil.ReadDir(INPUT_DIRECTORY)
+	if err != nil {
+		log.Fatal(err)
+	}
 	// scanner := bufio.NewScanner(f)
 	// posts := parsePosts(scanner)
-	writeToFile()
+	writeToFile(files)
 }
